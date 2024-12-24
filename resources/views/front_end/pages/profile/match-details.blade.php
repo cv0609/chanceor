49978faa8ee9b852a03b0b2e42b770575979136b
karan <kn@int.iqsay.com>
2024-12-24 12:58:18 +0530

    fixed

--- resources/views/front_end/pages/profile/match-details.blade.php
+++ resources/views/front_end/pages/profile/match-details.blade.php
@@ -1,58 +1,82 @@
 @extends('front_end.layout.main')
+@section('title','Match details')
 @section('content')
 
-<main class="main-content" id="main-content">
-   <!--  -->
-   <!--  -->
-   <!--  -->
-   <section class="constitutes">
-       <div class="container">
-           <div class="constitutes-inner">
-               <div class="back-btn">
-                   <a href="matches">
-                       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                           fill="#fff">
-                           <path d="M360-240 120-480l240-240 56 56-144 144h568v80H272l144 144-56 56Z"></path>
-                       </svg>
-                   </a>
-               </div>
-               <h2>2023-24 Season </h2>
+<!--Leagues Pages Banner Section Start -->
+<section class="pages-banner leagues-pages  bg-cover bg-no-repeat">
+    <div class="container">
+        <div class="banner-content">
+            <ul class="leagues-ul">
+                <li class="inline-list home-list">
+                    <h3>
+                        <a href="{{ route('home') }}">Home</a>
+                    </h3>
+                </li>
+                <li class="inline-list next-page-list">
+                    <h3>
+                        Match Details
+                    </h3>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!--Leagues Pages Banner Section End -->
 
-               <div class="palyer-header">
-                   <div class="team-logo">
-                       <div class="team_inner">
-                           <img src="{{ $matchDetails->home_team_logo ?? '' }}" alt="team-logo-4">
-                           {{-- <span> SHL</span> --}}
-                       </div>
-                       <h6>{{ $matchDetails->home_team_name ?? '' }}</h6>
-                   </div>
-                   <div class="match-details">
-                       {{-- <h6>Chase Stadium</h6>
-                       <p>Fort Lauderdale, Florida</p> --}}
-                       <span>{{ date('Y-m-d h:i a', strtotime($matchDetails->fixture_date ?? '')) }}</span>
-                   </div>
-                   <div class="team-logo">
-                       <div class="team_inner">
-                           {{-- <span>CNGZ </span> --}}
-                           <img src="{{ $matchDetails->away_team_logo ?? '' }}" alt="team-logo-2">
-                       </div>
-                       <h6>{{ $matchDetails->away_team_name ?? '' }}</h6>
-                   </div>
-               </div>
-               <div class="constitutes-inner-content">
-                   <div class="matches-table">
-                       <table cellpadding="0" cellspacing="0">
-                           <thead>
-                               <tr>
-                                   <th scope="col">Logo</th>
-                                   <th scope="col">Name</th>
-                                   <th scope="col">Position</th>
-                                   <th scope="col">Socure</th>
+<!-- Leagues Content Section Start -->
+<section class="basket_leagues match-details-page">
+    <div class="container">
+        <div class="basket_leagues-inner">
+            <div class="back-btn">
+                <a href="{{route('my-matches')}}" class="back-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                        fill="#fff">
+                        <path d="M360-240 120-480l240-240 56 56-144 144h568v80H272l144 144-56 56Z" />
+                    </svg>
+                </a>
+            </div>
+            <!-- Component Start -->
+            <h1 class="font-medium">
+                My Players
+            </h1>
+            <div class="flex flex-col mt-6">
+                <div class="my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="palyer-header">
+                            <div class="team-logo">
+                            <img src="{{ $matchDetails->home_team_logo ?? '' }}" alt="team-logo-4">
 
-                               </tr>
-                           </thead>
-                           <tbody>
-                              @foreach ($teams[0]->teamDetails ?? [] as $data)
+                                <h6>{{ $matchDetails->home_team_name ?? '' }}</h6>
+                            </div>
+                            <div class="match-details">
+                                <!-- 
+                                    <h6></h6>
+                                    <p></p>
+                                     -->
+                                <span>{{ date('Y-m-d h:i a', strtotime($matchDetails->fixture_date ?? '')) }}M</span>
+                            </div>
+
+                            <div class="team-logo">
+                            <img src="{{ $matchDetails->away_team_logo ?? '' }}" alt="team-logo-2">
+
+                                <h6>{{ $matchDetails->away_team_name ?? '' }}</h6>
+                            </div>
+                        </div>
+
+                        <div class="matches-table">
+                            <div class="div-tbl">
+                                <table class="table table-light table-striped table-hover table-sm">
+                                    <thead class=" text-uppercase">
+                                        <tr>
+                                            <th scope="col">Team</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Position</th>
+                                            <th scope="col">Score</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                    @foreach ($teams[0]->teamDetails ?? [] as $data)
                                <tr>
                                    <td class="match-team-logo">
                                        <a href="premier-league-players">
@@ -72,21 +96,22 @@
                                    </td>
                                </tr>
                                @endforeach
+                          
 
-                           </tbody>
-                       </table>
-                   </div>
-                   {{-- <div class="team-btns">
-                       <a href="team-view" class="next-btn">next</a>
-                   </div> --}}
-               </div>
-           </div>
-       </div>
-   </section>
-   <!--  -->
-</main>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
 
+                    </div>
+                </div>
+            </div>
 
+            <!-- Component End  -->
+        </div>
+    </div>
+</section>
+<!-- Leagues Content Section End -->
 @endsection
 
 @section('custom-script')
