8021eeedf850507f2efde69b93dd30b200ad2ef8
karan <kn@int.iqsay.com>
2024-12-24 16:09:39 +0530

    fixed

--- resources/views/front_end/pages/BasketBall/matches.blade.php
+++ resources/views/front_end/pages/BasketBall/matches.blade.php
@@ -28,7 +28,7 @@
     <div class="container">
         <div class="basket_leagues-inner">
             <div class="back-btn">
-                <a href="leagues">
+                <a href="{{ route('leagues') }}">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
                         fill="#fff">
                         <path d="M360-240 120-480l240-240 56 56-144 144h568v80H272l144 144-56 56Z"></path>
