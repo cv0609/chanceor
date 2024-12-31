45966a84a050ca06784a770452a3a06d2110d16c
nd@int.iqsay.com <nd@int.iqsay.com>
2024-12-31 10:49:18 +0530

    fixed

--- resources/views/admin/layout/head.blade.php
+++ resources/views/admin/layout/head.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Basketball</title>
+    <title>Chanceor</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
